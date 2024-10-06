b173d184a88ae21d2b78cd03324b6af6f931b420
Yuriy Grigoryev <allenvox@users.noreply.github.com>
2024-10-06 13:34:06 +0700

    Update db context with Employee-Task relations

--- lab-3/Data/ApplicationDbContext.cs
+++ lab-3/Data/ApplicationDbContext.cs
@@ -12,11 +12,25 @@ namespace lab_3.Data
 
         public DbSet<Project> Projects { get; set; }
         public DbSet<Employee> Employees { get; set; }
-        public DbSet<Task> Tasks { get; set; }
+        public DbSet<lab_3.Models.Task> Tasks { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
+
+            // Настройка отношений между Employee и Task
+            modelBuilder.Entity<lab_3.Models.Task>()
+                .HasOne(t => t.Executor)
+                .WithMany(e => e.Tasks)
+                .HasForeignKey(t => t.ExecutorId)
+                .OnDelete(DeleteBehavior.Restrict); // Укажите поведение при удалении, если это необходимо
+
+            modelBuilder.Entity<lab_3.Models.Task>()
+                .HasOne(t => t.Author)
+                .WithMany() // У автора нет коллекции задач, поэтому указываем WithMany()
+                .HasForeignKey(t => t.AuthorId)
+                .OnDelete(DeleteBehavior.Restrict);
         }
     }
 }
+
