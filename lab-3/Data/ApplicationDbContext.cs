e07ab8a134eb7a03056f9dd09f11bcc4e4e91d9a
Yuriy Grigoryev <allenvox@users.noreply.github.com>
2024-10-06 12:14:12 +0700

    Implement database context

---
+++ lab-3/Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+using Microsoft.EntityFrameworkCore;
+using lab_3.Models;
+
+namespace lab_3.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Project> Projects { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Task> Tasks { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
