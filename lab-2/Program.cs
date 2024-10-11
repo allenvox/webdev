bd21a9872e7a53d39a36ac084d609904fde83b3f
Yuriy Grigoryev <allenvox@users.noreply.github.com>
2024-10-12 00:32:18 +0700

    Modify the game to get all actions parameters from config

--- lab-2/Program.cs
+++ lab-2/Program.cs
@@ -1,14 +1,43 @@
 ﻿using System;
 using System.IO;
+using System.Text.Json;
+using System.Collections.Generic;
 
 class MarginalValera
 {
     // Параметры Валеры
-    public int Health { get; set; } = 100;
-    public int Alcohol { get; set; } = 0;
-    public int Joy { get; set; } = 0;
-    public int Fatigue { get; set; } = 0;
-    public int Money { get; set; } = 100;
+    public int Health { get; set; }
+    public int Alcohol { get; set; }
+    public int Joy { get; set; }
+    public int Fatigue { get; set; }
+    public int Money { get; set; }
+
+    // Конфигурация игры
+    private GameConfig config;
+
+    // Загрузка конфигурации из JSON
+    public void LoadConfig(string configFilePath)
+    {
+        if (File.Exists(configFilePath))
+        {
+            string json = File.ReadAllText(configFilePath);
+            config = JsonSerializer.Deserialize<GameConfig>(json);
+
+            // Инициализируем параметры Валеры из конфигурации
+            Health = config.InitialParameters.Health;
+            Alcohol = config.InitialParameters.Alcohol;
+            Joy = config.InitialParameters.Joy;
+            Fatigue = config.InitialParameters.Fatigue;
+            Money = config.InitialParameters.Money;
+
+            Console.WriteLine("Конфигурация игры загружена.");
+        }
+        else
+        {
+            Console.WriteLine("Файл конфигурации не найден.");
+            Environment.Exit(1);
+        }
+    }
 
     // Бесконечный цикл игры
     public void StartGame()
@@ -64,12 +93,12 @@ class MarginalValera
     // Действия
     public void GoToWork()
     {
-        if (Alcohol < 50 && Fatigue < 10)
+        if (Alcohol < config.Actions.GoToWork.RequiredAlcohol && Fatigue < config.Actions.GoToWork.RequiredFatigue)
         {
-            Joy -= 5;
-            Alcohol -= 30;
-            Money += 100;
-            Fatigue += 70;
+            Joy += config.Actions.GoToWork.JoyChange;
+            Alcohol += config.Actions.GoToWork.AlcoholChange;
+            Money += config.Actions.GoToWork.MoneyChange;
+            Fatigue += config.Actions.GoToWork.FatigueChange;
             Console.WriteLine("Валера пошел на работу.");
         }
         else
@@ -80,52 +109,52 @@ class MarginalValera
 
     public void ContemplateNature()
     {
-        Joy += 1;
-        Alcohol -= 10;
-        Fatigue += 10;
+        Joy += config.Actions.ContemplateNature.JoyChange;
+        Alcohol += config.Actions.ContemplateNature.AlcoholChange;
+        Fatigue += config.Actions.ContemplateNature.FatigueChange;
         Console.WriteLine("Валера созерцал природу.");
     }
 
     public void DrinkWineAndWatch()
     {
-        Joy -= 1;
-        Alcohol += 30;
-        Fatigue += 10;
-        Health -= 5;
-        Money -= 20;
+        Joy += config.Actions.DrinkWineAndWatch.JoyChange;
+        Alcohol += config.Actions.DrinkWineAndWatch.AlcoholChange;
+        Fatigue += config.Actions.DrinkWineAndWatch.FatigueChange;
+        Health += config.Actions.DrinkWineAndWatch.HealthChange;
+        Money += config.Actions.DrinkWineAndWatch.MoneyChange;
         Console.WriteLine("Валера пил вино и смотрел сериал.");
     }
 
     public void GoToBar()
     {
-        Joy += 1;
-        Alcohol += 60;
-        Fatigue += 40;
-        Health -= 10;
-        Money -= 100;
+        Joy += config.Actions.GoToBar.JoyChange;
+        Alcohol += config.Actions.GoToBar.AlcoholChange;
+        Fatigue += config.Actions.GoToBar.FatigueChange;
+        Health += config.Actions.GoToBar.HealthChange;
+        Money += config.Actions.GoToBar.MoneyChange;
         Console.WriteLine("Валера сходил в бар.");
     }
 
     public void DrinkWithMarginals()
     {
-        Joy += 5;
-        Health -= 80;
-        Alcohol += 90;
-        Fatigue += 80;
-        Money -= 150;
+        Joy += config.Actions.DrinkWithMarginals.JoyChange;
+        Health += config.Actions.DrinkWithMarginals.HealthChange;
+        Alcohol += config.Actions.DrinkWithMarginals.AlcoholChange;
+        Fatigue += config.Actions.DrinkWithMarginals.FatigueChange;
+        Money += config.Actions.DrinkWithMarginals.MoneyChange;
         Console.WriteLine("Валера пил с маргиналами.");
     }
 
     public void SingInSubway()
     {
-        Joy += 1;
-        Alcohol += 10;
-        Fatigue += 20;
-        Money += 10;
+        Joy += config.Actions.SingInSubway.JoyChange;
+        Alcohol += config.Actions.SingInSubway.AlcoholChange;
+        Fatigue += config.Actions.SingInSubway.FatigueChange;
+        Money += config.Actions.SingInSubway.MoneyChange;
 
-        if (Alcohol > 40 && Alcohol < 70)
+        if (Alcohol > config.Actions.SingInSubway.BonusAlcoholMin && Alcohol < config.Actions.SingInSubway.BonusAlcoholMax)
         {
-            Money += 50;
+            Money += config.Actions.SingInSubway.BonusMoney;
             Console.WriteLine("Валера пел в метро и заработал больше денег!");
         }
         else
@@ -136,20 +165,20 @@ class MarginalValera
 
     public void Sleep()
     {
-        if (Alcohol < 30)
+        if (Alcohol < config.Actions.Sleep.BadDreamsAlcoholLimit)
         {
-            Health += 90;
+            Health += config.Actions.Sleep.HealthGain;
             Console.WriteLine("Валера хорошо выспался.");
         }
 
-        if (Alcohol > 70)
+        if (Alcohol > config.Actions.Sleep.BadDreamsAlcoholLimit)
         {
-            Joy -= 3;
+            Joy -= config.Actions.Sleep.BadDreamsJoyPenalty;
             Console.WriteLine("Валере снились кошмары.");
         }
 
-        Alcohol -= 50;
-        Fatigue -= 70;
+        Alcohol += config.Actions.Sleep.AlcoholReduction;
+        Fatigue += config.Actions.Sleep.FatigueReduction;
     }
 
     // Сохранение игры в файл
@@ -188,11 +217,66 @@ class MarginalValera
     }
 }
 
+// Модель конфигурации
+public class GameConfig
+{
+    public InitialParameters InitialParameters { get; set; }
+    public Actions Actions { get; set; }
+}
+
+public class InitialParameters
+{
+    public int Health { get; set; }
+    public int Alcohol { get; set; }
+    public int Joy { get; set; }
+    public int Fatigue { get; set; }
+    public int Money { get; set; }
+}
+
+public class Actions
+{
+    public ActionConfig GoToWork { get; set; }
+    public ActionConfig ContemplateNature { get; set; }
+    public ActionConfig DrinkWineAndWatch { get; set; }
+    public ActionConfig GoToBar { get; set; }
+    public ActionConfig DrinkWithMarginals { get; set; }
+    public SingInSubwayConfig SingInSubway { get; set; }
+    public SleepConfig Sleep { get; set; }
+}
+
+public class ActionConfig
+{
+    public int RequiredAlcohol { get; set; } // Минимальный уровень алкоголя для выполнения действия
+    public int RequiredFatigue { get; set; } // Минимальный уровень усталости для выполнения действия
+    public int JoyChange { get; set; }
+    public int AlcoholChange { get; set; }
+    public int FatigueChange { get; set; }
+    public int MoneyChange { get; set; }
+    public int HealthChange { get; set; }
+}
+
+public class SingInSubwayConfig : ActionConfig
+{
+    public int BonusAlcoholMin { get; set; }
+    public int BonusAlcoholMax { get; set; }
+    public int BonusMoney { get; set; }
+}
+
+public class SleepConfig
+{
+    public int HealthGain { get; set; }
+    public int BadDreamsAlcoholLimit { get; set; }
+    public int BadDreamsJoyPenalty { get; set; }
+    public int AlcoholReduction { get; set; }
+    public int FatigueReduction { get; set; }
+}
+
 class Program
 {
     static void Main(string[] args)
     {
         MarginalValera valera = new MarginalValera();
+        valera.LoadConfig("./config.json");
         valera.StartGame();
     }
 }
